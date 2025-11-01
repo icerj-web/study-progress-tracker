4ad2bb0f2879962b1b23a7a35af15842f6cfb50e
icerj-web <icerj76@gmail.com>
2025-11-01 15:24:29 +0530

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,8 @@
+function hashPassword(str) { ... }
+function formatDate(dateStr) { ... }
+function calculateProgress(items) { ... }
+
+function saveUsers(users) { localStorage.setItem("users", JSON.stringify(users)); }
+function loadUsers() { ... }
+function saveMasterRoles(masterRoles) { localStorage.setItem("masterRoles", JSON.stringify(masterRoles)); }
+function loadMasterRoles() { ... }
